8548a05aefaebbea10fdc759eaa14589d5260549
miteshvaghasiya <mitesh.vaghasiya@gmail.com>
2015-09-02 20:37:58 +0530

    Update main.fs

--- New Project/main.fs
+++ New Project/main.fs
@@ -1,11 +1,23 @@
 (* This is a comment *)
 (* Sample Hello World program using F# *)
-printfn "Hello World!"
+(* printfn "Hello World!" *)
 
-as.n = [(i,j) | i <- [0...n-1], j<-[i...n-1]]
+let a = [10;12]
+printfn "%A" al
+
+(*
+
+let al = [10;20]
+let tp n = 1
+    for i = 0 to n-1 do
+        for j = i to n-1 do 
+            i+j
+
+[(i,j) | i <- [0...n-1], j<-[i...n-1]]
 --take.3.l
 --drop.n.l
 -- a!!i - is like a[i]
 segment.l.i.j = [l!!k | k <- [i...j]]
 allsegments.l = [segment.l.i.j | (i,j) <- as.(length.l)]
 sumas.l = [sum.s | s <- allsegments.l]
+*)

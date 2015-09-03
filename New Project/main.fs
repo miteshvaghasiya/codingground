63c109140d7e5f71b6672c68437ee5ad6265c464
miteshvaghasiya <mitesh.vaghasiya@gmail.com>
2015-09-03 20:35:06 +0530

    Update main.fs

--- New Project/main.fs
+++ New Project/main.fs
@@ -13,15 +13,33 @@ let pm x =
 pm 20.0f
 pm 10.0f
 pm -2.0f
-*)
 
 let sq x = x * x
 let l = [10.1;20.2;30.3]
 printfn "%f" (l |> List.map (fun x->x*x*x) |> List.sum)
 printfn "%d" ([1..100] |> List.filter (fun x->x%5=0) |> List.sum)
+*)
 
-qs [1;2;3;5;4;3;2;1]
 
+let rec qs = function
+    | [] -> []
+    | x::xs ->
+        let l, r = List.partition ((>=) x) xs
+        (qs l) @ [x] @ (qs r)
+
+
+printfn "%A" (qs [1;2;3;5;4;3;2;1])
+
+
+
+
+let rec qs_old l =
+    match l with
+        | [] -> []
+        | x::xs ->
+            let sml = xs |> List.filter (fun e -> e < x) |> qs
+            let lrl = xs |> List.filter (fun e -> e >= x) |> qs
+            sml @ [x] @ lrl
 
 
 
